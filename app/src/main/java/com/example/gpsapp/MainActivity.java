218c57a886ea8425d91ddb2b901d40b9fa4dda38
Dmytro Balabukh <demabalab@gmail.com>
2022-04-17 10:18:13 +0300

    added on/off update location

--- app/src/main/java/com/example/gpsapp/MainActivity.java
+++ app/src/main/java/com/example/gpsapp/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
+
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -16,26 +20,36 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int DEFAULT_CHECK_INTERVAL = 30000;
+    // Constants
+    public static final int DEFAULT_CHECK_INTERVAL = 10000;
     private static final int PERMISSION_FINE_LOCATION = 95;
-    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_updates
-            ,tv_address;
+
+    // UI Elements
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_updates, tv_address;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     Switch sw_locationsupdates;
 
     LocationRequest locationRequest;
+
+    // API Client
     FusedLocationProviderClient fusedLocationProviderClient;
+
+    // Callback that is called whenever location is requested/removed.
+    LocationCallback locationCallBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Setting UI elements
         tv_lat = findViewById(R.id.tv_lat);
         tv_lon = findViewById(R.id.tv_lon);
         tv_altitude = findViewById(R.id.tv_altitude);
@@ -43,22 +57,58 @@ public class MainActivity extends AppCompatActivity {
         tv_speed = findViewById(R.id.tv_speed);
         tv_updates = findViewById(R.id.tv_updates);
         tv_address = findViewById(R.id.tv_address);
-
         sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
 
+        //Setting location request to update every 10 sec and be very accurate
         locationRequest = new com.google.android.gms.location.LocationRequest();
         locationRequest.setInterval(DEFAULT_CHECK_INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-//        sw_gps.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(sw_gps.isChecked()){
-//                    locationRequest.setPriority(locationRequest.PRIORITY_HIGH_ACCURACY)
-//                }
-//            }
-//        });
+        // Setting location callaback
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                updateUI(locationResult.getLastLocation());
+            }
+        };
 
+        // Setting listener when "updates" switch is toggled.
+        sw_locationsupdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (sw_locationsupdates.isChecked()) {
+                    startLocationUpdate();
+                } else {
+                    stopLocationUpdate();
+                }
+            }
+        });
+
+        //Update GPS on start of the app
+        updateGPS();
+    }
+
+    // Stop getting location updates if updates switch is off.
+    private void stopLocationUpdate() {
+        tv_updates.setText("Location is not tracking.");
+        tv_altitude.setText("Location is not tracking.");
+        tv_accuracy.setText("Location is not tracking.");
+        tv_lon.setText("Location is not tracking.");
+        tv_address.setText("Location is not tracking.");
+        tv_lat.setText("Location is not tracking.");
+        tv_speed.setText("Location is not tracking.");
+        tv_updates.setText("Off");
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
+
+    // start getting location updates if switch is on, but before that check for permissions.
+    private void startLocationUpdate() {
+        tv_updates.setText("On");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
         updateGPS();
     }
 
@@ -80,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateGPS() {
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        //check if you have permissions to get location, if so, then get last location and update ui
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
@@ -89,13 +140,15 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         else {
-            //BUILD 23(OS) or highger, build code for 23 is M
+            // Request location permissions
+            // BUILD 23(OS) or highger, build code for 23 is M
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
             }
         }
     }
 
+    // Update UI by getting the latest location updates info
     private void updateUI(Location location) {
         tv_lat.setText(String.valueOf(location.getLatitude()));
         tv_lon.setText(String.valueOf(location.getLongitude()));
